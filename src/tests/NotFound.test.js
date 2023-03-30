2ede78a1bbb220eaa3b3f00bd8d4f166eb0edc70
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-30 15:19:04 -0300

    Feat: Requisito 04 Resolvido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testa o componente NotFound', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const h2El = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(h2El).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const ImageEl = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(ImageEl.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
