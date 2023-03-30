42f15a4e3d060bb0d8369fb3cec8bd775d6b9473
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-30 15:41:25 -0300

    Fix: Requisito 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,4 +1,4 @@
-import { getAllByTestId, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
